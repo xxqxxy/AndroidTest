e0ab881727c33e724188942d57e3dd482fe7bc39
xxqxxy <xq_android_forever@163.com>
2017-07-05 11:59:28 +0800

    commit helloActivity

---
+++ app/src/main/java/com/xq/androidtest/HelloActivity.java
@@ -0,0 +1,17 @@
+package com.xq.androidtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by xxq on 2017/7/5.
+ */
+
+public class HelloActivity  extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
